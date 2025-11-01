143e70c73506ee70d74c0ae326b552f0fd69862e
MaybeTarun <tarunthegreat2003@gmail.com>
2025-11-01 19:43:13 +0530

    removed auto deploy

--- src/components/ValueProposition.tsx
+++ src/components/ValueProposition.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { motion, scale } from "framer-motion";
+import { motion } from "framer-motion";
 
 const ValueProposition: React.FC = () => {
   const features = [
